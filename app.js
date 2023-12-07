8cc8555d6d98366b2cb353648b52e5a3ac9e6c0c
MacFlick <mackk5504@gmail.com>
2023-12-08 02:24:55 +0630

    Update Web Ui

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-/* eslint-disable no-unused-vars */
 const mainMenu = document.querySelector('.navbar');
 const closeMenu = document.getElementById('menu-icon2');
 const openMenu = document.getElementById('menu-icon1');
-// eslint-disable-next-line camelcase
-const menu_items = document.querySelectorAll('.navbar li a');
+const closeIcon = document.querySelector('.fa-xmark');
+const opeIcon = document.querySelector('.fa-bars');
 
-// close menu when you click on a menu item
 function show() {
+  closeIcon.style.display = 'block';
+  opeIcon.style.display = 'none';
+
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
-  openMenu.style.display = 'none';
-  closeMenu.style.display = 'block';
 }
 function close() {
   mainMenu.style.top = '-440px';
-  openMenu.style.display = 'block';
-  closeMenu.style.display = 'none';
+  closeIcon.style.display = 'none';
+  opeIcon.style.display = 'block';
 }
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
-// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef, no-unused-vars
 const swiper = new Swiper('.home ', {
   spaceBetween: 30,
   centeredSlides: true,
